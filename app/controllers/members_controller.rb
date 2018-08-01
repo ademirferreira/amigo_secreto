6c59a4703b0902c57433640153d70d9b7c649d92
Ademir Ferreira <ademir.ae86@gmail.com>
2018-07-31 22:22:14 -0300

    include members coffee, update members controller  and model member

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -7,10 +7,10 @@ class MembersController < ApplicationController
 
   def create
     @member = Member.new(member_params)
-
+  
     respond_to do |format|
       if @member.save
-        format.json { render json: true }
+        format.json { render json: @member }
       else
         format.json { render json: @member.errors, status: :unprocessable_entity }
       end
