c4c0c7d48d8e33843902f4639a4790fe3664ceeb
Ademir Ferreira <ademir.ae86@gmail.com>
2018-08-02 19:23:48 -0300

    configuration the production enviroment

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ require 'sidekiq/web'
 
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
-  mount Sidekiq::Web => '/sidekiq'
+  #mount Sidekiq::Web => '/sidekiq'
 
   root to: 'pages#home'
   resources :campaigns, except: [:new] do
